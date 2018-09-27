b34d246a7f7a6384792e700a0566517753ac5124
tommycrestanello <tommy.crest@libero.it>
2018-09-27 15:45:49 +0200

    Add files via upload

---
+++ www_ua/script/admiral.js
@@ -0,0 +1,23 @@
+/* admiral.js : main javascript for Grand Admiral Resort & SPA */
+function sideSlide() {
+	$('#sidebar').toggleClass('active');
+	$(this).toggleClass('active');
+}
+
+$(document).ready(function () {
+
+    $("#sidebar").mCustomScrollbar({
+         theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        // open or close navbar
+        $('#sidebar').toggleClass('active');
+        // close dropdowns
+        $('.collapse.in').toggleClass('in');
+        // and also adjust aria-expanded attributes we use for the open/closed arrows
+        // in our CSS
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+});
